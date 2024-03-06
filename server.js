b136f4674eefaca8bedfb0c14f4ce0633a054749
SamiRZL <102523018+SamiRZL@users.noreply.github.com>
2024-03-06 16:15:06 +0100

    fix cors

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 app.use(cors({
     origin: '*',
     credentials: true,
-})); app.use('/public', express.static("files"));
+})); app.use('/', express.static("public"));
 app.use('/documents', documentRoutes);
 app.use('/categories', categoriesRoutes);
 app.use('/aspects', aspectsRoutes);
